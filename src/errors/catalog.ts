06714a01f1b1304e7ccc8d99c7ffb02ad906ea87
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-14 20:00:33 -0300

    feat: criado arquivo com catalago de erros; erros "objectNotFound" e "idLengthNotAllowed" adicionados

---
+++ src/errors/catalog.ts
@@ -0,0 +1,26 @@
+import httpStatusCode from "../helpers/httpStatusCode"
+
+export enum ErrorTypes {
+  objectNotFound = 'objectNotFound',
+  idLengthNotAllowed = 'idLengthNotAllowed',
+}
+
+type ErrorObject = {
+  message: string,
+  httpStatus: number,
+}
+
+export type ErrorCatalog = {
+  [key in ErrorTypes]: ErrorObject;
+}
+
+export const errorCatalog: ErrorCatalog = {
+  objectNotFound: {
+    message: 'Object not found',
+    httpStatus: httpStatusCode.NOT_FOUND,
+  },
+  idLengthNotAllowed: {
+    message: 'Id must have 24 hexadecimal characters',
+    httpStatus: httpStatusCode.BAD_REQUEST,
+  },
+}
