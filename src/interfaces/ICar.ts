1ffcf058df28c4aac50f8774572aad380c0c5be7
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-14 15:24:21 -0300

    feat: Requisito 3 - criada interface ICar a partir da interface IVehicle

---
+++ src/interfaces/ICar.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+import { vehicleZodSchema } from "./IVehicle";
+
+const carZodSchema = vehicleZodSchema.extend({
+  doorsQty: z.number().int().min(2).max(4),
+  seatsQty: z.number().int().min(2).max(7),
+})
+
+type ICar = z.infer<typeof carZodSchema>;
+
+export {
+  carZodSchema,
+  ICar,
+}
