6300978ddf6a3cc42ddd97ee12067928dd873219
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-14 16:43:43 -0300

    feat: Requisito 4 - criada classe CarService; criado método "create" para a rota "POST /cars"

---
+++ src/services/Car.service.ts
@@ -0,0 +1,34 @@
+import { carZodSchema, ICar } from "../interfaces/ICar";
+import { IModel } from "../interfaces/IModel";
+import { IService } from "../interfaces/IService";
+import CarModel from "../models/Car.model";
+
+class CarService implements IService<ICar> {
+  constructor(private model: IModel<ICar>) {}
+
+  public async create(obj: unknown): Promise<ICar> {
+    const parsed = carZodSchema.safeParse(obj);
+
+    if (!parsed.success) throw parsed.error;
+
+    return this.model.create(obj as ICar);
+  }
+
+  public async read(): Promise<ICar[]> {
+    throw new Error("Method not implemented.");
+  }
+
+  public async readOne(_id: string): Promise<ICar> {
+    throw new Error("Method not implemented.");
+  }
+
+  public async update(_id: string, obj: unknown): Promise<ICar> {
+    throw new Error("Method not implemented.");
+  }
+
+  public async delete(_id: string): Promise<ICar> {
+    throw new Error("Method not implemented.");
+  }
+}
+
+export default CarService;
