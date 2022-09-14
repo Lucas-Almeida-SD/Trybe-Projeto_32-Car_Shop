67435f4f06091a8027755937e873ca164031026a
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-14 19:04:41 -0300

    test: Requisito 5 - criado arquivo de testes para "CarModel"; criado testes para o método "create" de CarModel

---
+++ src/tests/unit/models/Car.test.ts
@@ -0,0 +1,35 @@
+import * as sinon from 'sinon';
+import chai from 'chai';
+import { Model } from 'mongoose';
+import * as carMock from '../mocks/carMock';
+import CarModel from '../../../models/Car.model';
+
+const { expect } = chai;
+
+describe('Testes de CarModel', () => {
+  const carModel = new CarModel();
+
+  describe('Método "create', () => {
+
+    describe('Quando a criação do carro ocorre com sucesso', () => {
+      before(async () => {
+        sinon
+          .stub(Model, 'create')
+          .resolves(carMock.validCarWithId);
+      });
+    
+      after(()=>{
+        sinon.restore();
+      })
+    
+      it('Retorna o objeto do carro criado', async () => {
+        const result = await carModel.create(carMock.validCar);
+        
+        expect(result).to.be.eqls(carMock.validCarWithId);
+      });
+    });
+    
+  });
+
+
+});
