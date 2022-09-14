af96a2061dc8b4fcb42a0235a5eb14f5bd6ac429
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-14 16:50:13 -0300

    feat: Requisito 4 -carRoute e errorMiddleware adicionados ao app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import express from 'express';
+import 'express-async-errors';
+import errorMiddleware from './middlewares/errorMiddleware';
+import carRoute from './routes/Car.route';
 
 const app = express();
+app.use(express.json());
+app.use(carRoute);
+
+app.use(errorMiddleware);
 
 export default app;
