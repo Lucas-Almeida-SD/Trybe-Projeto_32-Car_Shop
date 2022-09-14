cf23fd3373abecf880b693130d57406db22c9e27
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-14 15:06:34 -0300

    feat: Requisito 2 - criada interface IVehicle e seu objeto de validação com zod

---
+++ src/interfaces/IVehicle.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+const vehicleZodSchema = z.object({
+  model: z.string().min(3),
+  year: z.number().int().min(1900).max(2022),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number().int(),
+});
+
+type IVehicle = z.infer<typeof vehicleZodSchema>;
+
+export {
+  vehicleZodSchema,
+  IVehicle,
+}
