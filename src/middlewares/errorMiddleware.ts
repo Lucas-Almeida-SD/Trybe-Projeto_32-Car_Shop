1cf0ccfd47d55c54512cf94b01b8e848677970fa
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-14 20:03:26 -0300

    feat: tratamento de erro do catalogo de erros

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -1,5 +1,6 @@
 import { ErrorRequestHandler } from "express"
 import { ZodError } from "zod"
+import { errorCatalog, ErrorTypes } from "../errors/catalog"
 import httpStatusCode from "../helpers/httpStatusCode"
 
 const errorMiddleware: ErrorRequestHandler = (
@@ -10,6 +11,13 @@ const errorMiddleware: ErrorRequestHandler = (
 ) => {
   if (err instanceof ZodError) {
     return res.status(httpStatusCode.BAD_REQUEST).json({ message: err.issues })
+  }
+
+  const messageAsErrorType = err.message as keyof typeof ErrorTypes;
+  const mappedError = errorCatalog[messageAsErrorType];
+  if(mappedError) {
+    const { httpStatus, message } = mappedError;
+    return res.status(httpStatus).json({ error: message });
   }
 
   return res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({ message: err.message });
