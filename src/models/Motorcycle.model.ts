73a8aae3b61956df02b9c3ecb765689b607eaa1d
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-15 15:54:21 -0300

    feat: Requisito 19 - criada classe MotorcycleModel para ser a camada model do recurso "/motorcycles"

---
+++ src/models/Motorcycle.model.ts
@@ -0,0 +1,21 @@
+import { model as createModel, Schema } from 'mongoose';
+import { IMotorcycle } from '../interfaces/IMotorcycle';
+import MongoModel from './MongoModel';
+
+const motorcycleMongooseSchema = new Schema<IMotorcycle>({
+  model: String,
+  year: Number,
+  color: String,
+  status: Boolean,
+  buyValue: Number,
+  category: String,
+  engineCapacity: Number,
+});
+
+class MotorcycleModel extends MongoModel<IMotorcycle> {
+  constructor(model = createModel('Motorcycle', motorcycleMongooseSchema)) {
+    super(model);
+  }
+}
+
+export default MotorcycleModel;
