b3f44e1b8bbd316213d0a003eab6bb18f90a87c9
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-15 15:57:51 -0300

    feat: Requisito 19 - motorcycleRoute adicionado a API

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import 'express-async-errors';
 import errorMiddleware from './middlewares/errorMiddleware';
 import carRoute from './routes/Car.route';
+import motorcycleRoute from './routes/Motorcycle.route';
 
 const app = express();
 app.use(express.json());
 app.use(carRoute);
+app.use(motorcycleRoute);
 
 app.use(errorMiddleware);
 
