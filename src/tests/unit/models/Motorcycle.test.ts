bbd2d6281aecdd7fa897d15e66573e2481311537
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-15 18:20:28 -0300

    test: criado testes para o método "delete" de MotorcycleModel

--- src/tests/unit/models/Motorcycle.test.ts
+++ src/tests/unit/models/Motorcycle.test.ts
@@ -110,6 +110,38 @@ describe.only('Testes de MotorcycleModel', () => {
         expect(result).to.be.eqls(null);
       });
     });
-    
+  });
+
+  describe('Método "delete"', () => {
+
+    before(() => {
+      sinon.stub(Model, 'findOneAndDelete')
+      .onCall(0).resolves(motorcycleMock.validMotorcycleWithId)
+      .onCall(1).resolves(null);
+    });
+
+    after(() => [
+      sinon.restore()
+    ]);
+
+    describe('Quando a moto é deletada com sucesso', () => {
+      it('Retorna o objeto da moto deletada', async () => {
+        const result = await motorcycleModel.delete(
+          motorcycleMock.validMotorcycleWithId._id,
+        );
+
+        expect(result).to.be.eqls(motorcycleMock.validMotorcycleWithId);
+      });
+    });
+
+    describe('Quando o id é inexistente', () => {
+      it('Retorna valor nulo', async () => {
+        const result = await motorcycleModel.delete(
+          '999999999999999999999999',
+        );
+
+        expect(result).to.be.eqls(null);
+      });
+    });
   });
 });
