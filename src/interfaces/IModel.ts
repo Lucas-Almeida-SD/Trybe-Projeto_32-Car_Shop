0f9205432f2585001419900e1e76cd3c669a8d31
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-15 00:14:20 -0300

    chore: alterado type do parametro "obj" do método "update"

--- src/interfaces/IModel.ts
+++ src/interfaces/IModel.ts
@@ -1,8 +1,10 @@
+import { UpdateQuery } from "mongoose";
+
 interface IModel<T> {
   create(obj: T): Promise<T>;
   read(): Promise<T[]>;
   readOne(_id: string): Promise<T | null>;
-  update(_id: string, obj: T): Promise<T | null>;
+  update(_id: string, obj: UpdateQuery<T>): Promise<T | null>;
   delete(_id: string): Promise<T | null>;
 }
 
