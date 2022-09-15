af72432ef2c17b5525a29d6306908a59f7476749
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-15 12:10:46 -0300

    test: criado testes para o método "update" de CarModel

--- src/tests/unit/models/Car.test.ts
+++ src/tests/unit/models/Car.test.ts
@@ -78,4 +78,33 @@ describe('Testes de CarModel', () => {
       expect(result).to.be.eqls(null);
     });
   });
+
+  describe('Método "update"', () => {
+
+    before(() => {
+      sinon.stub(Model, 'findOneAndUpdate')
+      .onCall(0).resolves(carMock.updatedCarWithId)
+      .onCall(1).resolves(null);
+    });
+
+    after(() => {
+      sinon.restore();
+    });
+
+    describe('Quando o carro é atualizado com sucesso', () => {
+      it('Retorna o objeto do carro atualizado', async () => {
+        const result = await carModel.update(carMock.validCarWithId._id, carMock.updatedCar);
+  
+        expect(result).to.be.eqls(carMock.updatedCarWithId);
+      });
+    });
+
+    describe('Quando o id não existe', () => {
+      it('Retorna valor nulo', async () => {
+        const result = await carModel.update('999999999999999999999999', carMock.updatedCar);
+  
+        expect(result).to.be.eqls(null);
+      });
+    })
+  });
 });
