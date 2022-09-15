f73e5df0c454b5f02ac5c7d6f529e3bbb13c6cdc
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-15 12:14:22 -0300

    test: criado testes para o método "delete" de CarModel

--- src/tests/unit/models/Car.test.ts
+++ src/tests/unit/models/Car.test.ts
@@ -107,4 +107,33 @@ describe('Testes de CarModel', () => {
       });
     })
   });
+
+  describe('Método "delete"', () => {
+
+    before(() => {
+      sinon.stub(Model, 'findOneAndDelete')
+      .onCall(0).resolves(carMock.validCarWithId)
+      .onCall(1).resolves(null);
+    });
+
+    after(() => {
+      sinon.restore();
+    });
+
+    describe('Quando o carro é deletado com sucesso', () => {
+      it('Retorna o objeto do carro deletado', async () => {
+        const result = await carModel.delete(carMock.validCarWithId._id);
+  
+        expect(result).to.be.eqls(carMock.validCarWithId);
+      });
+    });
+
+    describe('Quando o id não existe', () => {
+      it('Retorna valor nulo', async () => {
+        const result = await carModel.delete('999999999999999999999999');
+  
+        expect(result).to.be.eqls(null);
+      });
+    })
+  });
 });
