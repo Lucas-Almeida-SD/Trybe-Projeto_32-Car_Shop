eb9cbca0ce8e7ff6d2d67b0dccdda39ad13a04d1
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-15 15:57:13 -0300

    feat: Requisito 19 - criada rota "POST /motorcycles"

---
+++ src/routes/Motorcycle.route.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import MotorcycleModel from '../models/Motorcycle.model';
+import MotorcycleService from '../services/Motorcycle.service';
+import MotorcycleController from '../controllers/Motorcycle.controller';
+
+const motorcycleModel = new MotorcycleModel();
+const motorcycleService = new MotorcycleService(motorcycleModel);
+const motorcycleController = new MotorcycleController(motorcycleService);
+
+const motorcycleRoute = Router();
+
+motorcycleRoute.post('/motorcycles', (req, res) => motorcycleController.create(req, res));
+
+export default motorcycleRoute;
