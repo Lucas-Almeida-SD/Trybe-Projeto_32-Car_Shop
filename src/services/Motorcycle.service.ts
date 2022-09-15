ef1cd6c0c30c2af21ebf778e388e8adccdec5942
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-15 17:17:48 -0300

    feat: Requisito 23 - criado método "delete" de MotorcycleService para camada service da rota "DELETE /motorcycles/:id"

--- src/services/Motorcycle.service.ts
+++ src/services/Motorcycle.service.ts
@@ -48,8 +48,14 @@ class MotorcycleService implements IService<IMotorcycle> {
     return updatedMotorcycle;
   }
 
-  delete(_id: string): Promise<{ status?: boolean | undefined; model: string; year: number; color: string; buyValue: number; category: 'Street' | 'Custom' | 'Trail'; engineCapacity: number; }> {
-    throw new Error('Method not implemented.');
+  public async delete(_id: string): Promise<IMotorcycle> {
+    MotorcycleValidate.validateIdLength(_id);
+
+    const removedMotorcycle = await this.model.delete(_id);
+
+    if (!removedMotorcycle) throw new Error(ErrorTypes.objectNotFound);
+
+    return removedMotorcycle;
   }
 }
 
