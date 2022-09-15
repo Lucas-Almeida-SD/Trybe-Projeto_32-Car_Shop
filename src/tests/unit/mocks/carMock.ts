44bcf842dd4e43a7d4196980580584a47d4e19b6
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-15 12:09:42 -0300

    feat: criado mock "updatedCarWithId"

--- src/tests/unit/mocks/carMock.ts
+++ src/tests/unit/mocks/carMock.ts
@@ -26,6 +26,16 @@ export const updatedCar = {
   doorsQty: 2
 };
 
+export const updatedCarWithId = {
+  _id: '62cf1fc6498565d94eba52cd',
+  model: 'Uno da Escada',
+  year: 1966,
+  color: 'blue',
+  buyValue: 3500,
+  seatsQty: 2,
+  doorsQty: 2
+};
+
 export const coverageCar = {
   model: 'Uno da Escada',
   year: 1963,
