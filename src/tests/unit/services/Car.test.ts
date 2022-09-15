e7b269170299ed93e927c5931b17a06393e8d16b
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-14 23:04:22 -0300

    feat: Requisito 11 - criado teste para o método "readOne" de CarService

--- src/tests/unit/services/Car.test.ts
+++ src/tests/unit/services/Car.test.ts
@@ -147,5 +147,56 @@ describe('Testes de CarService', () => {
     });
   });
 
+  describe('Método "readOne"', () => {
 
+    describe('Quando a leitura ocorre com sucesso', () => {
+
+      before(() => {
+        sinon.stub(carModel, 'readOne').resolves(carMock.validCarWithId);
+      });
+  
+      after(() => {
+        sinon.restore();
+      });
+  
+      it('É possível listar um carro com sucesso através do id', async () => {
+        const result = await carService.readOne(carMock.validCarWithId._id);
+  
+        expect(result).to.be.eqls(carMock.validCarWithId);
+      });
+    });
+
+    describe('Quando a leitura NÃO ocorre com sucesso', () => {
+
+      describe('Caso o id possua menos que 24 caracteres', () => {
+
+        it('Lança o erro "idLengthNotAllowed"', async () => {
+          try {
+            await carService.readOne('12345678901234567892123');
+          } catch(err) {
+            expect((err as Error).message).to.be.equal('idLengthNotAllowed');
+          }
+        });
+      });
+
+      describe('Caso o id possua 24 caracteres mas é inválido', () => {
+
+        before(() => {
+          sinon.stub(carModel, 'readOne').resolves(null);
+        });
+    
+        after(() => {
+          sinon.restore();
+        });
+
+        it('Lança o erro "idLengthNotAllowed"', async () => {
+          try {
+            await carService.readOne('999999999999999999999999');
+          } catch(err) {
+            expect((err as Error).message).to.be.equal('objectNotFound');
+          }
+        });
+      });
+    });
+  })
 });
