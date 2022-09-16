faad7f5bd6a033846815bb5198309e5a42e641cd
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-16 11:23:25 -0300

    test: criado arquivo com os testes do método "read" de CarController

---
+++ src/tests/unit/controllers/Car.read.test.ts
@@ -0,0 +1,44 @@
+import sinon from 'sinon';
+import { expect } from 'chai';
+import CarModel from '../../../models/Car.model';
+import CarService from '../../../services/Car.service';
+import CarController from '../../../controllers/Car.controller';
+import * as carMock from '../mocks/carMock';
+import { Request, Response } from 'express';
+
+describe('Testes de CarController', () => {
+  const carModel = new CarModel();
+  const carService = new CarService(carModel);
+  const carController = new CarController(carService);
+
+  const req = {} as Request;
+  const res = {} as Response;
+
+  before(() => {
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns(res);
+    res.end = sinon.stub().returns(res);
+  });
+
+  after(()=>{
+    sinon.restore();
+  });
+
+  describe('Método "read"', () => {
+    before(() => {
+      sinon.stub(carService, 'read').resolves([carMock.validCarWithId]);
+    });
+
+    it('Retorna status 200', async () => {
+      await carController.read(req, res);
+
+      expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+    });
+
+    it('Retorna uma lista de carros no corpo da response', async () => {
+      await carController.read(req, res);
+
+    expect((res.json as sinon.SinonStub).calledWith([carMock.validCarWithId])).to.be.true;
+    });
+  });
+});
