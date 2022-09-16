29babcb448016f15ef216848d2df0b8ac76d54c1
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-16 11:51:14 -0300

    chore: arquivos deletados pois possuíam validações iguais, então foi criado um arquivo único para essas validações em comum

--- src/validations/Car.validate.ts
+++
@@ -1,7 +0,0 @@
-import { ErrorTypes } from '../errors/catalog';
-
-export default abstract class CarValidate {
-  public static validateIdLength(_id: string) {
-    if (_id.length < 24) throw new Error(ErrorTypes.idLengthNotAllowed);
-  }
-}
--- src/validations/Motorcycle.validate.ts
+++
@@ -1,7 +0,0 @@
-import { ErrorTypes } from '../errors/catalog';
-
-export default abstract class MotorcycleValidate {
-  public static validateIdLength(_id: string) {
-    if (_id.length < 24) throw new Error(ErrorTypes.idLengthNotAllowed);
-  }
-}
