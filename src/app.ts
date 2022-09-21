ad424d5b08a1f526ad07bf204ebc9c2841cc464b
lucas_almeida <lucas.almdiniz@gmail.com>
2022-09-20 23:19:08 -0300

    feat: adicionada rota para docmentação da API

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from '../swagger.output.json';
 import errorMiddleware from './middlewares/errorMiddleware';
 import carRoute from './routes/Car.route';
 import motorcycleRoute from './routes/Motorcycle.route';
@@ -8,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(carRoute);
 app.use(motorcycleRoute);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(errorMiddleware);
 
